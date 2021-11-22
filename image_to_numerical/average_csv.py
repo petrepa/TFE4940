c2605f897cf739dea5c2fd877d7f3857492fd0fe
Peter Remøy Paulsen <30476405+petrepa@users.noreply.github.com>
2021-11-22 16:17:43 +0100

    Added average row to end of each csv file

--- image_to_numerical/average_csv.py
+++ image_to_numerical/average_csv.py
@@ -1,33 +1,46 @@
 import csv
+import os
 
-# directory = './results'
+directory = 'results/'
 
-# for filename in directory:
-#     with open(filename, 'rb') as f:
-#         reader = csv.reader(f)
-        
-#         next(reader) # to skip header
+for filename in os.listdir(directory):
+    file = directory + filename
+    
+    with open(file, 'r') as f:
+        reader = csv.reader(f)
+        header = next(reader)
+        data_list = list(reader)
+        rows = [''] + ['{:.15f}'.format(sum(float(x) for x in y) / len(data_list)) for y in list(zip(*data_list))[1:]] # with a 15 decimal accuracy
+        average_data_list = [header] + [rows]
 
-#         numbers = [float(row[range(1, 6)]) for row in reader]
+        print(rows)
 
-#         average = sum(numbers)/len(numbers)
+        rows[0] = 'Average'
 
-
-with open('./results/FG_Counting-Fingers_BG_Complex_copy.csv', 'r') as f:
-    reader = csv.reader(f)
-    header = next(reader)
-    data_list = list(reader)
-    rows = [''] + ['{:.15f}'.format(sum(float(x) for x in y) / len(data_list)) for y in list(zip(*data_list))[1:]] # with a 15 decimal accuracy
-    average_data_list = [header] + [rows]
-
-    print(rows)
-
-    rows[0] = 'Average'
-
-    print(rows)
+        print(rows)
 
 
-    # append the new data to the file
-    with open('./results/FG_Counting-Fingers_BG_Complex_copy.csv', 'a', newline='') as file:
-        writer = csv.writer(file)
-        writer.writerow(rows)
+        # append the new data to the file
+        with open(file, 'a', newline='') as file_write:
+            writer = csv.writer(file_write)
+            writer.writerow(rows)
+
+
+# with open('results/FG_Counting-Fingers_BG_Window.csv', 'r') as f:
+#     reader = csv.reader(f)
+#     header = next(reader)
+#     data_list = list(reader)
+#     rows = [''] + ['{:.15f}'.format(sum(float(x) for x in y) / len(data_list)) for y in list(zip(*data_list))[1:]] # with a 15 decimal accuracy
+#     average_data_list = [header] + [rows]
+
+#     print(rows)
+
+#     rows[0] = 'Average'
+
+#     print(rows)
+
+
+#     # append the new data to the file
+#     with open('results/FG_Counting-Fingers_BG_Window.csv', 'a', newline='') as file:
+#         writer = csv.writer(file)
+#         writer.writerow(rows)
